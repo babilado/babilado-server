1f7e868924a68735b1cc551ec14afd248f634403
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-19 10:30:09 +1100

    Use threadpool to handle more than one connection at once

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 use babilado_types::Event;
 use jsonl::Connection;
-use std::net::TcpListener;
+use std::io;
+use std::net::{TcpListener, TcpStream};
 
 fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:9999")?;
+    let pool = threadpool::Builder::new().build();
 
     for stream in listener.incoming() {
-        let stream = stream?;
-        let mut connection = Connection::new_from_tcp_stream(stream)?;
-
-        loop {
-            let event: Event = connection.read()?;
-            dbg!(&event);
-            connection.write(&event)?;
-        }
+        pool.execute(|| {
+            if let Err(e) = handle_connection(stream) {
+                eprintln!("Error: {:?}", e);
+            }
+        });
     }
 
+    pool.join();
+
     Ok(())
+}
+
+fn handle_connection(stream: io::Result<TcpStream>) -> anyhow::Result<()> {
+    let stream = stream?;
+    let mut connection = Connection::new_from_tcp_stream(stream)?;
+
+    loop {
+        let event: Event = connection.read()?;
+        dbg!(&event);
+        connection.write(&event)?;
+    }
 }
