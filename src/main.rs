ccd63b04f7bfe62054cc60dfd3e10d33a62674a0
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-19 09:49:00 +1100

    Create basic echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use babilado_types::Event;
+use jsonl::Connection;
+use std::io::BufReader;
+use std::net::TcpListener;
+
+fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:9999")?;
+
+    for stream in listener.incoming() {
+        let stream = stream?;
+        let mut connection = Connection::new(BufReader::new(stream.try_clone()?), stream);
+
+        loop {
+            let event: Event = connection.read()?;
+            dbg!(&event);
+            connection.write(&event)?;
+        }
+    }
+
+    Ok(())
 }
