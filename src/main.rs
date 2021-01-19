bcb1f45e341eac07d5715f369da02a64275e48b6
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-19 16:36:34 +1100

    Rewrite using Tokio
    This fixes the bug where only as many clients as threads could connect
    to the server.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,30 @@
 use babilado_types::Event;
 use jsonl::Connection;
-use std::io;
-use std::net::{TcpListener, TcpStream};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::{io, task};
 
-fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:9999")?;
-    let pool = threadpool::Builder::new().build();
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:9999").await?;
 
-    for stream in listener.incoming() {
-        pool.execute(|| {
-            if let Err(e) = handle_connection(stream) {
+    loop {
+        let stream = listener.accept().await.map(|(stream, _)| stream);
+
+        task::spawn(async {
+            if let Err(e) = handle_connection(stream).await {
                 eprintln!("Error: {:?}", e);
             }
         });
     }
-
-    pool.join();
-
-    Ok(())
 }
 
-fn handle_connection(stream: io::Result<TcpStream>) -> anyhow::Result<()> {
-    let stream = stream?;
-    let mut connection = Connection::new_from_tcp_stream(stream)?;
+async fn handle_connection(stream: io::Result<TcpStream>) -> anyhow::Result<()> {
+    let mut stream = stream?;
+    let mut connection = Connection::new_from_tcp_stream(&mut stream)?;
 
     loop {
-        let event: Event = connection.read()?;
+        let event: Event = connection.read().await?;
         dbg!(&event);
-        connection.write(&event)?;
+        connection.write(&event).await?;
     }
 }
