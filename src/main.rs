7a73a2e43538ece64607e0ecea55d6cb65fbe0a7
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-19 18:27:11 +1100

    Forward events as they arrive to all clients

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,64 @@
 use babilado_types::Event;
-use jsonl::Connection;
+use tokio::io::{self, BufReader};
+use tokio::net::tcp::OwnedWriteHalf;
 use tokio::net::{TcpListener, TcpStream};
-use tokio::{io, task};
+use tokio::sync::mpsc;
+use tokio::{select, task};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let (clients_tx, clients_rx) = mpsc::channel(100);
+    let (events_tx, events_rx) = mpsc::channel(100);
+    task::spawn(forward_events(clients_rx, events_rx));
+
     let listener = TcpListener::bind("127.0.0.1:9999").await?;
 
     loop {
         let stream = listener.accept().await.map(|(stream, _)| stream);
 
+        let clients_tx = clients_tx.clone();
+        let events_tx = events_tx.clone();
+
         task::spawn(async {
-            if let Err(e) = handle_connection(stream).await {
+            if let Err(e) = handle_stream(stream, clients_tx, events_tx).await {
                 eprintln!("Error: {:?}", e);
             }
         });
     }
 }
 
-async fn handle_connection(stream: io::Result<TcpStream>) -> anyhow::Result<()> {
-    let mut stream = stream?;
-    let mut connection = Connection::new_from_tcp_stream(&mut stream)?;
+async fn forward_events(
+    mut clients_rx: mpsc::Receiver<OwnedWriteHalf>,
+    mut events_rx: mpsc::Receiver<Event>,
+) -> anyhow::Result<()> {
+    let mut clients = Vec::new();
 
     loop {
-        let event: Event = connection.read().await?;
-        dbg!(&event);
-        connection.write(&event).await?;
+        select! {
+            Some(client) = clients_rx.recv() => clients.push(client),
+            Some(event) = events_rx.recv() => {
+                for client in &mut clients {
+                    jsonl::write(client, &event).await?;
+                }
+            },
+            else => return Ok(()),
+        }
+    }
+}
+
+async fn handle_stream(
+    stream: io::Result<TcpStream>,
+    clients_tx: mpsc::Sender<OwnedWriteHalf>,
+    events_tx: mpsc::Sender<Event>,
+) -> anyhow::Result<()> {
+    let stream = stream?;
+    let (read_half, write_half) = stream.into_split();
+
+    clients_tx.send(write_half).await.unwrap();
+
+    let mut read_half = BufReader::new(read_half);
+    loop {
+        let event: Event = jsonl::read(&mut read_half).await?;
+        events_tx.send(event).await.unwrap();
     }
 }
